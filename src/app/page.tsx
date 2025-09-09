740a91c704bc5f1f1b346d10435086a4f58b426a
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-09-09 14:09:45 +0530

    Added the document for the routing purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button"
 const home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center font-mono text-3xl">
       <h1>Welcome to the Home Page</h1>
+      <Button className="ml-4" variant="default">
+        click me
+      </Button>
     </div>
   )
 }
