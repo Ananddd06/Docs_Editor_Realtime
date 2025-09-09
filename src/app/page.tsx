c7ea166c1b724a5bfd1f2d749ac8c4efad09b614
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-09-09 10:23:30 +0530

    Added the first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-mono text-3xl">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  )
+}
+
+export default home
